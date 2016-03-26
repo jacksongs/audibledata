672758f29edfff6f20553cfec8ee2a9ea5ce856e
jacksongs <jacksongsimac@Jacksons-iMac.local>
2016-03-26 13:22:34 +1100

    Testing

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import requests
 import scraperwiki
 import simplejson
 
-baseurl = "https://api.audible.com.au/1.0/catalog/products?response_groups=product_plans,rating,product_extended_attrs,contributors%2Cproduct_desc,product_attrs&num_results=5&products_sort_by=BestSellers&page="
+baseurl = "https://api.audible.com.au/1.0/catalog/products?response_groups=product_plans,rating,product_extended_attrs,contributors%2Cproduct_desc,product_attrs&num_results=50&products_sort_by=BestSellers&page="
 
 page = 0
 rank = 1
@@ -15,22 +15,29 @@ while True:
 	if len(jsonresults['products']) == 0:
 		break
 	for product in jsonresults['products']:
-		print product.keys()
-		print rank
-		print product['asin']
-		print product['rating']
-		print product['issue_date']
-		print product['release_date']
-		print product['copyright']
+		prod = {
+				'rank': rank,
+		 		'title': product['title'],
+		 		'asin': product['asin'],
+		 		'reviews': product['rating']['num_reviews'],
+				'orating': product['rating']['overall_distribution']['average_rating'],
+				'srating': product['rating']['story_distribution']['average_rating'],
+				'prating': product['rating']['performance_distribution']['average_rating'],
+		 		'issue_date': product['issue_date'],
+		 		'release_date': product['release_date'],
+			 	'copyright': product['copyright'],
+		 		'runtime_length_min': product['runtime_length_min'],
+			 	'format_type': product['format_type'],
+				'publisher_name': product['publisher_name']
+		}
 		try:
-			print product['publication_name']
+			prod['publication_name'] = product['publication_name']
 		except:
-			print 'NO NAME PROVIDED'
-		print product['runtime_length_min']
-		print product['format_type']
+			pass
+		print prod
 		rank+=1
 	page += 1
-	if page > 3:
+	if page > 10:
 		break
 
 
